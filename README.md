5fa6178706fa25c6ae0e98784f397b7b3364f93a
B660M-G\Administrator <3@qq.com>
2025-11-25 19:14:13 +0800

    springboot 启动成功

--- README.md
+++
@@ -1,2 +0,0 @@
-# servlet_spring_boot
-spring_boot 封装了servlet及ssm，一统天下！
---
+++ src/test/java/org/kunggea/servlet_spring_boot/ServletSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kunggea.servlet_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
