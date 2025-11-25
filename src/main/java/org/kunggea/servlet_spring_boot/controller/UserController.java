4ac6aaca542f9b7939c66126d0f1597d30337ef7
B660M-G\Administrator <3@qq.com>
2025-11-25 21:41:15 +0800

    spring boot <version>2.7.18</version>  using Java 1.8.0_392 jar包可运行了

--- src/main/java/org/kunggea/servlet_spring_boot/controller/UserController.java
+++ src/main/java/org/kunggea/servlet_spring_boot/controller/UserController.java
@@ -17,6 +17,9 @@ public class UserController {
 
     @GetMapping("/findAllUsers")
     public List<User> findAllUser() {
+        System.out.println("  amazon JDK 1.8 can do with \n spring boot 2.7.18");
+        System.out.println("  spring boot <version>2.7.18</version>");
+        System.out.println("  using Java 1.8.0_392");
         return userService.findAllUser();
     }
 }
